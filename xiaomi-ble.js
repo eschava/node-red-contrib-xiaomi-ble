d0add3fe5abb7a55067b5d7a1c324fb972e549d1
novalu <novalu@novalu.info>
2019-09-20 20:33:28 +0200

    Change noble dependency to @abandonware/noble as former package is unmaintained

--- xiaomi-ble.js
+++ xiaomi-ble.js
@@ -1,7 +1,7 @@
 module.exports = function(RED) {
     "use strict";
 
-    var noble = require('noble');
+    var noble = require('@abandonware/noble');
 	
     function XiaomiBleNode(config) {
         RED.nodes.createNode(this, config);
